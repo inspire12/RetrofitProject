fa6ce14e6cb9e70e5aa4411144cb39655ada4cf4
서영학(Yeonghak Seo) <inspire12@ncsoft.com>
2018-08-03 14:26:35 +0900

    Add Retrotit builder

--- app/src/main/java/com/example/inspire12/retrofitproject/MainActivity.kt
+++ app/src/main/java/com/example/inspire12/retrofitproject/MainActivity.kt
@@ -2,13 +2,17 @@ package com.example.inspire12.retrofitproject
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
-
+    val baseUrl = "http://demo2587971.mockable.io/"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        var retrofit : Retrofit = Retrofit.Builder()
+                .baseUrl(baseUrl).addConverterFactory(GsonConverterFactory.create())
+                .build()
     }
 }
